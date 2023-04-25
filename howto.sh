8bb5c8eca9da753a77bb922e30da653f06814947
alexk49 <alexk49@users.noreply.github.com>
2023-04-25 08:54:07 +0100

    first commit of howto.sh

---
+++ howto.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+howtofile = '/repos/howto/howto.txt'
+
+for arg in "$@"; do
+   grep "$arg" howtofile 
+done
+
+# search for commands
+search_for_commands () {
+    grep -E 'command:.*$2' howtofile
+}
+
+# grep -E 'expr1.*expr2|expr2.*expr1'
+
+# search for description 
+search_for_description () {
+    grep -E 'description:.*$2' howtofile
+}
