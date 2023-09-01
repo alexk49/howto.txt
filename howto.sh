eb36a671214a115e97289192425b63f15b1f229b
alexk49 <alexk49@users.noreply.github.com>
2023-09-01 19:22:32 +0100

    updated addition so that it no longer uses headings

--- howto.sh
+++ howto.sh
@@ -11,17 +11,11 @@ if [[ "$#" -eq 0 ]]; then
     exit 1
 fi
 
-if [ "$1" == "-a" ]; then
-    if [ "$#" -ne 3 ]; then
-        echo "Invalid number of args given for appending."
-        echo "Append useage is: ht -a [where-to-add-text] [text-to-add]"
-        exit 1
-    fi
-    heading="$2"
-    addition="$3"
+if [[ "$1" == "-a" ]]; then
+    addition="$2"
 
-    echo "Adding $addition under heading: # $heading"
-    sed -i -e "/\#[[:space:]]$heading/a $addition" "$howtofilepath"
+    echo "Adding $addition under to $howtofilepath"
+    echo "$addition" >> "$howtofilepath"
 else
 
     search_query="$1"
